72677f28045c3a17a5e0bbd0294a189b514a6049
Nicoletha <nicolgastelum5@gmail.com>
2024-10-25 14:49:07 -0700

    examen dda

---
+++ Examen-DDA/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Examen_DDA
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Sumar(object sender, EventArgs e)
+        {
+            if (double.TryParse(numero1.Text, out double num1) && double.TryParse(numero2.Text, out double num2))
+            {
+                double resultado = num1 + num2;
+                resultadoLabel.Text = $"Resultado: {resultado}";
+            }
+        }
+
+        private void Restar(object sender, EventArgs e)
+        {
+            if (double.TryParse(numero1.Text, out double num1) && double.TryParse(numero2.Text, out double num2))
+            {
+                double resultado = num1 - num2;
+                resultadoLabel.Text = $"Resultado: {resultado}";
+            }
+        }
+
+        private void Multiplicar(object sender, EventArgs e)
+        {
+            if (double.TryParse(numero1.Text, out double num1) && double.TryParse(numero2.Text, out double num2))
+            {
+                double resultado = num1 * num2;
+                resultadoLabel.Text = $"Resultado: {resultado}";
+            }
+        }
+
+        private void Dividir(object sender, EventArgs e)
+        {
+            if (double.TryParse(numero1.Text, out double num1) && double.TryParse(numero2.Text, out double num2))
+            {
+                if (num2 != 0)
+                {
+                    double resultado = num1 / num2;
+                    resultadoLabel.Text = $"Resultado: {resultado}";
+                }
+                else
+                {
+                    resultadoLabel.Text = "No se puede dividir por cero.";
+                }
+            }
+        }
+    } 
+}
+
+
+
+
